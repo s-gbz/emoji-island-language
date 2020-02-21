54222d3bad9b541735433e8eb613a82e4dda50d6
Sergej Grilborzer <sergej1995@gmail.com>
2020-02-21 09:40:59 +0100

    Customize stack machine

--- runtime/src/main/java/runtime/StackMachine.java
+++ runtime/src/main/java/runtime/StackMachine.java
@@ -1,4 +1,33 @@
 package runtime;
 
+import java.util.Stack;
+import runtime.StackElement.Type;
+
 public class StackMachine {
+    private Stack<StackElement> stack;
+    private StackElement[] programmMemory;
+
+    private void eql() {
+        StackElement arg1 = stack.pop();
+        StackElement arg2 = stack.pop();
+
+        if(arg1.getType() == Type.) {
+            arg1 = programmMemory[(int) arg1.getValue()];
+        }
+
+        if(arg2.getType() == Type.ADDRESS) {
+            arg2 = programmMemory[(int) arg2.getValue()];
+        }
+
+        if(arg1.equals(arg2)) {
+            pushBoolean(true);
+        }else {
+            pushBoolean(false);
+        }
+    }
+
+    private void pushBoolean(boolean booleanArg) {
+        StackElement element = new StackElement(Type.BOOLEAN, booleanArg);
+        stack.push(element);
+    }
 }
